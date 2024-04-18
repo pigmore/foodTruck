244938917b232669b1c08b13d012b0697c6e311b
pigmore <pigmore@gmail.com>
2024-04-18 14:05:34 +0800

    optimize query result by geohash

--- src/App.js
+++ src/App.js
@@ -2,28 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 import { initMap,infoMarker } from "./component/mapMaker";
-import { db } from "./component/firebase";
-import { collection, getDocs, query, limit} from "firebase/firestore";
+import { db,doQueryByGeo } from "./component/firebase";
 
 function App() {
-
-  async function doQuery() {
-    const q = query(
-      collection(db, "foodTrucks"),
-      limit(20)
-    );
-    const querySnapshot = await getDocs(q);
-    console.log('querySnapshot',querySnapshot)
-
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    });
-  }
 
   useEffect(() => {
     initMap()
     infoMarker()
-    doQuery()
+    // doQueryByGeo()
   },[])
 
   return (
--- src/component/firebase.js
+++ src/component/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { collection, addDoc,getDocs,query, orderBy, startAt,endAt,limit,writeBatch,doc,where } from "firebase/firestore";
+import {geohashForLocation,geohashQueryBounds,distanceBetween} from 'geofire-common';
 const firebaseConfig = {
   apiKey: "AIzaSyBPdrlaHEUypbmCH75-eyNPbRwsD-HNJM4",
   authDomain: "foodtruck-84ff0.firebaseapp.com",
@@ -12,3 +13,40 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+export async function doQueryByGeo(la=37.7749295,ln=-122.4194155,dis=5*100) {
+  const center = [la, ln];
+  const bounds = geohashQueryBounds(center, dis);
+  const promises = [];
+  for (const b of bounds) {
+    const q = query(
+      collection(db, 'foodTrucks'),
+      where("Status", "==", 'APPROVED'),
+      orderBy('geohash'),
+
+      startAt(b[0]),
+      endAt(b[1]));
+
+    promises.push(getDocs(q));
+  }
+
+  const snapshots = await Promise.all(promises);
+
+  console.log(snapshots)
+  const matchingDocs = [];
+  const points = [];
+  for (const snap of snapshots) {
+    for (const doc of snap.docs) {
+      const lat = doc.get('Latitude');
+      const lng = doc.get('Longitude');
+      const distanceInKm = distanceBetween([lat, lng], center);
+      const distanceInM = distanceInKm * 100;
+      if (distanceInM <= dis ) {
+        matchingDocs.push(doc);
+        points.push([lng,lat])
+      }
+    }
+  }
+  console.log(matchingDocs)
+  return [matchingDocs,points]
+}
