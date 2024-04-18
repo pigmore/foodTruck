50e8c983a77eb4dc8f181ba41cb4514550ccb804
pigmore <pigmore@gmail.com>
2024-04-18 13:16:48 +0800

    initMap + set the center

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const centerPoint = [-122.4194155,37.7749295]
+  function initMap(){
+    window.map = new window.BMapGL.Map("allmap");
+    window.map.centerAndZoom(new window.BMapGL.Point(...centerPoint), 15);
+    window.map.enableScrollWheelZoom(true);
+  }
+
+  useEffect(() => {
+    initMap()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      id="allmap"
+      style={{height:'100vh'}}
+    />
   );
 }
 
