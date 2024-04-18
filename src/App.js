96d52342b0483a5a43437cc11f39395595270ba0
pigmore <pigmore@gmail.com>
2024-04-18 13:21:58 +0800

    Try make some info window

--- src/App.js
+++ src/App.js
@@ -10,8 +10,28 @@ function App() {
     window.map.enableScrollWheelZoom(true);
   }
 
+  function infoMarker() {
+
+    var marker = new window.BMapGL.Marker(new window.BMapGL.Point(...centerPoint));
+    window.map.addOverlay(marker);
+    // 创建图文信息窗口
+    var sContent = `<h4 style='margin:0 0 5px 0;'>Title</h4>
+        <p style='margin:0;line-height:1.5;font-size:13px;text-indent:2em'>
+        Contents...Contents...Contents...
+        </p></div>`;
+    var infoWindow = new window.BMapGL.InfoWindow(sContent);
+    // marker添加点击事件
+    marker.addEventListener('click', function () {
+        this.openInfoWindow(infoWindow);
+    });
+    marker.addEventListener('ontouchstart', function () {
+        this.openInfoWindow(infoWindow);
+    });
+  }
+
   useEffect(() => {
     initMap()
+    infoMarker()
   },[])
 
   return (
